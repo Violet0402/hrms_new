12f15cbfd24fc29dd9e866f83129dfb3702392c1
sumaojin <sumaojin@meb.com>
2020-09-24 15:49:44 +0800

    first

---
+++ hrms-java/src/main/java/com/sumaojin/suzhenping/hrms/mapper/AccountMapper.java
@@ -0,0 +1,18 @@
+package com.sumaojin.suzhenping.hrms.mapper;
+
+import com.sumaojin.suzhenping.hrms.entity.Account;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.mapstruct.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author SMJ
+ * @since 2020-09-17
+ */
+@Mapper
+public interface AccountMapper extends BaseMapper<Account> {
+
+}
---
+++ hrms-java/src/main/java/com/sumaojin/suzhenping/hrms/service/IAccountService.java
@@ -0,0 +1,16 @@
+package com.sumaojin.suzhenping.hrms.service;
+
+import com.sumaojin.suzhenping.hrms.entity.Account;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author SMJ
+ * @since 2020-09-17
+ */
+public interface IAccountService extends IService<Account> {
+
+}
